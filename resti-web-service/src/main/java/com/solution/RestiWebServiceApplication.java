ac727b440e49c4efbe7c6c2485af03411485787a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-20 12:38:44 +0530

    rest-service

---
+++ resti-web-service/src/main/java/com/solution/RestiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestiWebServiceApplication.class, args);
+	}
+
+}
